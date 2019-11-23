91a80176f22f954142435026bc101d3b57412477
llorenspujol <llorenspujol1@gmail.com>
2019-11-23 12:31:12 +0100

    Replaced Jasmine/Karma for Jest & upgraded Typescript version to 3.7

--- src/app/spec/utils.spec.ts
+++
@@ -1,7 +0,0 @@
-import {} from "jasmine";
-
-describe('Utils Test', () => {
-    it('Test if test works... great', () => {
-        expect(0).toBe(0);
-    })
-});
---
+++ src/app/test/utils.test.ts
@@ -0,0 +1,5 @@
+describe('Utils Test', () => {
+    test('Test if test works... great', () => {
+        expect(0).toBe(0);
+    })
+});
