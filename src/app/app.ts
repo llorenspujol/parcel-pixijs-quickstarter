ff574d559e95b6f995a573e1ba489bce630394bd
Watson121 <adamtedders@outlook.com>
2022-07-21 17:40:35 +0100

    Initial Commit
    Made changes so that I can draw shapes

--- src/app/app.ts
+++ src/app/app.ts
@@ -14,46 +14,69 @@ const playerFrames: BomberFrames = bomberFrames;
 // IMPORTANT: Change this value in order to see the Hot Module Reloading!
 const currentFrame: keyof BomberFrames = 'front';
 
+// Graphics Class
+var graphics = new PIXI.Graphics();
+
+const gravity = 8.31;
 
 export class GameApp {
 
     private app: PIXI.Application;
 
+    private shape : Shape;
+
     constructor(parent: HTMLElement, width: number, height: number) {
 
         this.app = new PIXI.Application({width, height, backgroundColor : 0x000000});
         parent.replaceChild(this.app.view, parent.lastElementChild); // Hack for parcel HMR
+        graphics = new PIXI.Graphics();
+        
+        const newshape = new Shape(200, 100, this.getApp());
+        this.shape = newshape;
 
-        // init Pixi loader
-        let loader = new PIXI.Loader();
-
-        // Add user player assets
-        console.log('Player to load', playerFrames);
-        Object.keys(playerFrames).forEach(key => {
-            loader.add(playerFrames[key]);
-        });
-
-        // Load assets
-        loader.load(this.onAssetsLoaded.bind(this));
     }
 
-    private onAssetsLoaded() {
+    public getApp(){
+        return this.app;
+    }    
 
-        const playerIdle: PIXI.AnimatedSprite = new PIXI.AnimatedSprite(playerFrames[currentFrame].map(path => PIXI.Texture.from(path)));
-
-        /*
-        * An AnimatedSprite inherits all the properties of a PIXI sprite
-        * so you can change its position, its anchor, mask it, etc
-        */
-        playerIdle.x = 100;
-        playerIdle.y = 150;
-        playerIdle['vx'] = 1;
-        playerIdle.anchor.set(0, 1);
-        // playerIdle.anchor.set(0.5);
-        playerIdle.animationSpeed = 0.3;
-        playerIdle.play();
-
-        this.app.stage.addChild(playerIdle);
+    public update(delta){
+        this.shape.Move(delta);
     }
 
+    public SetShape(shape : Shape){
+        this.shape = shape;
+    }
+
+}
+
+export class Shape {
+    private x : number;
+    private y : number;
+
+    constructor(x: number, y : number, app: PIXI.Application){
+        this.x = x;
+        this.y = y;
+
+        graphics.beginFill(0xDE3249);
+        graphics.drawRect(50, 50, x, y);
+        graphics.endFill();
+        app.stage.addChild(graphics);
+
+        
+    }
+
+    public Move(delta){
+        this.y += delta * 1 * gravity;
+        graphics.y = this.y;
+        
+        if(this.y > window.innerHeight){
+            this.x = Math.random() * (window.innerHeight + 50);
+            this.y = window.innerHeight - (window.innerHeight + 250);
+            graphics.x = this.x;
+            graphics.y = this.y;
+        }
+        
+
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
-import {GameApp} from "./app/app";
+import {GameApp, Shape} from "./app/app";
 
 const myGame = new GameApp(document.body,  window.innerWidth, window.innerHeight);
+
+
+requestAnimationFrame(Update);
+
+function Update(){
+    myGame.update(1);
+    requestAnimationFrame(Update);
+}
